bf2b87e7f710330e8aeaf41b20b903a220a36561
Ankur Shahi <shahiankur80@gmail.com>
2021-11-14 14:07:15 -0500

    JavaScript code to get the player's robot name

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,9 @@
-window.alert("This is an alert!JavaScript is running!");
+// create a function named "fight"
+let playerName = window.prompt("What is your robot's name?");
+
+console.log(playerName);
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+// fight();
